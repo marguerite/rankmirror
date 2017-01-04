d9cf214e22584f706a585f2b34f5728861226a2e
marguerite <i@marguerite.su>
2017-01-04 23:58:21 +0800

    use cache system to boost the performance. tree watching instead of xpath, faster

---
+++ lib/rankmirror/cache.rb
@@ -0,0 +1,51 @@
+require 'curb'
+require 'nokogiri'
+module RankMirror
+	class Cache
+		def initialize(uri)
+			@uri = uri
+			@host = @uri.sub("http://","")
+			@filename = File.join("/tmp",@host)
+		end
+
+		def fetch
+			unless is_recent?
+				buffer = open(@filename,'w')
+				r = Curl::Easy.new(@uri)
+				r.on_body do |b|
+					buffer.write b
+				end
+				r.perform
+				buffer.close
+				to_xml
+			end
+			return @filename + ".xml"
+		end
+
+		def to_xml
+			buffer = open(@filename) {|f|
+					f.read
+				}
+			doc = Nokogiri::XML(buffer)
+			f = open(@filename + ".xml",'w')
+			doc.write_xml_to(f)
+			f.close
+		end
+
+		def is_recent?
+			if File.exist?(@filename + ".xml")
+				last_time = File.mtime(@filename + ".xml")
+				# one week
+				if Time.now - last_time < 60*60*24*7
+					true
+				else
+					false
+				end
+			else
+				false
+			end
+		end
+
+	end
+end
+
--- lib/rankmirror/remoteoss.rb
+++ lib/rankmirror/remoteoss.rb
@@ -1,113 +1,50 @@
 require 'nokogiri'
-require 'open-uri'
-require 'ostruct'
 
 module RankMirror
 	class RemoteOSS
-        
 		def initialize(options)
-			oss = Nokogiri::HTML(open("http://mirrors.opensuse.org"))
-			@mirrors = Array.new
-			continent = String.new
-
-			oss.xpath("//table/tbody/tr").each do |tr|
-                    		# <tr>
-                    		#   <td colspan="31" class="newregion">Africa:</td>
-                    		# </tr>
-                    		unless tr.xpath("td").attribute("class").nil?
-                            		continent = tr.xpath("td").text.gsub(":","")
-					case continent
-					when "Africa"
-						continent = "africa"
-					when "Europe"
-						continent = "euro"
-					when "North America"
-						continent = "na"
-					when "South America"
-						continent = "sa"
-					when "Oceania"
-						continent = "ocean"
-					else
-						continent = "asia"
-					end
-                    		else
-                            		# 0 <td>Ecuador</td>
-                            		# 1 <td><a href="http://www.cedia.org.ec">Consorcio Ecuatoriano para el Desarrollo de Internet Avanzado</a></td>
-                            		# 2 <td><a href="http://mirror.cedia.org.ec/opensuse">HTTP</a></td>
-                            		# 3 <td><a href="ftp://mirror.cedia.org.ec/opensuse/">FTP</a></td>
-                            		# 4 <td><a href="rsync://mirror.cedia.org.ec/opensuse/">rsync</a></td>
-                            		# 5 <td>***</td>
-                            		# 6 <td class="a"></td>
-                            		# 7 <td class="b"></td> TW
-                            		# 8 <td class="a">√</td>
-                            		# 9 <td class="b">√</td> 422
-                            		# 10 <td class="a"></td>
-                            		# 11 <td class="b"></td>
-                            		# 12 <td class="a">√</td> 422 update 
-                            		# 13 <td class="b">√</td>
-                            		# 14 <td class="a">√</td> 421
-                            		# 15 <td class="b"></td>
-                            		# 16 <td class="a"></td>
-                            		# 17 <td class="b">√</td> 421 update
-                            		td0 = tr.at_xpath("td[1]").text
-                            		if td0.length > 0 # ignore the placeholders
-                                    		td1,td2,td3 = tr.at_xpath("td[2]/a").text,nil,nil
-                                    		td2 = tr.at_xpath("td[3]/a/@href").value unless tr.at_xpath("td[3]/a").nil?
-                                    		td3 = tr.at_xpath("td[4]/a/@href").value unless tr.at_xpath("td[4]/a").nil? 
-                                    		td5 = tr.at_xpath("td[6]").text.length
-                                    		td7 = tr.at_xpath("td[8]").text.length
-                                    		td9 = tr.at_xpath("td[10]").text.length
-                                    		td14 = tr.at_xpath("td[15]").text.length
-
-						if options.continent == "world" || options.continent == continent
-							mirror = OpenStruct.new
-							mirror.continent = continent
-							mirror.country = tr.at_xpath("td[1]").text.strip!
-                                    			if td1.length != 0
-								mirror.name = td1
-                                    			else
-								mirror.name = td2
-                                    			end
-							if td2.nil?
-								mirror.http = td3
-							else
-								mirror.http = td2
-							end
-							mirror.ftp = td3
-							mirror.priority = td5
-                                    			if td7 == 0
-								mirror.tumbleweed = false
-							else
-								mirror.tumbleweed = true
-							end
-							if td9 == 0
-								mirror.leap4220 = false
-							else
-								mirror.leap4220 = true
-							end
-							if td14 == 0
-                                    				mirror.leap4210 = false
-							else
-								mirror.leap4210 = true
-							end
-
-							if mirror.tumbleweed == false && mirror.leap4220 == false && mirror.leap4210 == false
-								status = RankMirror::Status.new(mirror.http).get(options)
-								["tumbleweed","leap4420","leap4210","leap4230"].each {|i| mirror[i] = status[i]} unless status.nil?
-							end
-
-							@mirrors << mirror
-						end
-                            		end
-                    		end
-            		end
-			@mirrors = @mirrors.compact
-        	end
-
-		def sort(options)
-			sorted = @mirrors.map!{|mirror| mirror.http if mirror[options.flavor]}.compact!
+			@mirrors = []
+			@continent = ""
+			@options = options
 		end
 
-    end
-end
+		def fetch
+			cache = RankMirror::Cache.new("http://mirrors.opensuse.org").fetch
+			buffer = open(cache) {|f|
+					f.read
+				}
+			doc = Nokogiri::XML.parse(buffer)
+			doc.root.children.children.children.select{|l| l.name == "tr"}.each do |tr|
+				unless tr.children[1].attribute("class").nil?
+					@continent = tr.children[1].inner_text.delete!(":").delete("\s").downcase!
+				else
+					if @options.continent == "world" || @options.continent == @continent
+						country = tr.children[1].inner_text.strip!
+						unless country.nil?
+							tumbleweed = tr.children[15].children[0].nil? ? false : true
+							leap4220 = tr.children[17].children[0].nil? ? false : true
+							leap4210 = tr.children[27].children[0].nil? ? false : true
 
+							ftpobj = tr.children[7].children[0]
+							ftp = ftpobj.nil? ? nil : ftpobj.attribute("href").inner_text
+							httpobj = tr.children[5].children[0]
+							http = httpobj.nil? ? ftp : httpobj.attribute("href").inner_text
+
+							unless leap4210 || leap4220 || tumbleweed
+								status = RankMirror::Status.new(http).get(@options)
+								unless status.nil?
+									tumbleweed = status["tumbleweed"]
+									leap4220 = status["leap4220"]
+									leap4210 = status["leap4210"]
+								end
+							end
+
+							@mirrors << http if eval(@options.flavor)
+						end
+					end
+				end
+			end
+			return @mirrors
+		end
+	end
+end
